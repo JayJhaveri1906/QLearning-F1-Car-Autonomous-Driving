8b230b53fbd3a5c658250f1abd38018100d65ebb
JayJhaveri1906 <jjhaveri1906@gmail.com>
2023-02-22 22:48:31 -0800

    Added Movement controls, Zandvoort map, Co-authored-by: Andrew Ghafari <ang08@mail.aub.edu>

--- carGame.py
+++ carGame.py
@@ -8,11 +8,11 @@ from pygame_util import scale_image, rotate_image_ByCenter
 
 # MAIN()
 # Loading image assets
-BG = pygame.image.load("assets/bg_lava.png")
-BG = scale_image(BG, 1.5) # resizing shiz to fit
+BG = pygame.image.load("assets/bg_ucsd.png")
+# BG = scale_image(BG, 1.5) # resizing shiz to fit
 
-TRACK = pygame.image.load("assets/track_600.png")
-TRACK_OL = pygame.image.load("assets/track_outline_600.png")
+TRACK = pygame.image.load("assets/track_Zandvoort.png")
+TRACK_OL = pygame.image.load("assets/track_outline_Zandvoort.png")
 FINISH = pygame.image.load("assets/finishLine.png")
 FINISH = scale_image(FINISH, 0.2)
 
@@ -34,12 +34,36 @@ class AbstractCar:  # Abstract parent class of a car
         self.rot_vel = rot_vel # how quickly you can turn
         self.angle = -70 # starting angle
         self.x, self.y = self.StartPos
+        self.acc = 0.1 # px/s
 
-    def rotate(self, left = False, right = False):
+    def rotate(self, left = False, right = False): # Rotate the car
         if left:
             self.angle += self.rot_vel
         elif right:
             self.angle -= self.rot_vel
+
+    def move_fwd(self):
+        self.vel = min(self.vel + self.acc, self.max_vel) # acceerate the car
+        self.move()
+
+    def move_bkwd(self):
+        self.vel = max(self.vel - self.acc, -self.max_vel/(1.5)) # acceerate the car
+        self.move()
+
+    def move(self):
+        radians = math.radians(self.angle)
+        yvel = math.cos(radians) * self.vel
+        xvel = math.sin(radians) * self.vel
+        self.x -= xvel  # - because of the perspective...
+        self.y -= yvel
+
+    def fwdFriction(self):
+        self.vel = max(self.vel - (self.acc/4), 0) # Forward friction
+        self.move()
+
+    def bkwdFriction(self):
+        self.vel = min(self.vel + (self.acc/4), 0) # Backward friction
+        self.move()
 
     def draw(self, win):
         rotate_image_ByCenter(win, self.img, (self.x, self.y), self.angle)
@@ -54,13 +78,43 @@ def draw(win, images, humanCar):
         win.blit(img, posi)
 
     humanCar.draw(win) # Draw the car
+    pygame.display.set_caption(str(round(clock.get_fps())))
     pygame.display.update()  # Update display after new drawing
 
 
-human1 = HumanPlayer(4,4)
+lastMove = 0 # 0: fwd, 1: bkwd  ## Used to remember which the last movement was, accordingly apply friction.
+def movePlayer(player):
+    global lastMove
+
+    keys = pygame.key.get_pressed()
+
+    moved = False
+
+    if keys[pygame.K_a]:
+        player.rotate(left=True)
+    if keys[pygame.K_d]:
+        player.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True
+        lastMove = 0
+        player.move_fwd()
+    if keys[pygame.K_s]:
+        moved = True
+        lastMove = 1
+        player.move_bkwd()
+
+    if not moved:
+        if lastMove == 0:
+            player.fwdFriction()
+        if lastMove == 1:
+            player.bkwdFriction()
+
+
+human1 = HumanPlayer(1.5, 2)
 
 FPS = 60
 clock = pygame.time.Clock() # To sync FPS ## no jittery
+
 
 run = True
 while run: # handles all events (collisions, user movements, window status, etc)
@@ -74,5 +128,8 @@ while run: # handles all events (collisions, user movements, window status, etc)
         if event.type == pygame.QUIT: # X is clicked?
             run = False
             break
+
+    movePlayer(human1)
+
 
 pygame.quit()
