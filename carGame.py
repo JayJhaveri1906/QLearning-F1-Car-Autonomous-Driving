ba76ff98586641ca1b4e9c1f72288f287dad8306
JayJhaveri1906 <jjhaveri1906@gmail.com>
2023-02-22 20:03:14 -0800

    Created Basic Car UI

---
+++ carGame.py
@@ -0,0 +1,78 @@
+import pygame
+import time
+import math
+
+# util files
+from pygame_util import scale_image, rotate_image_ByCenter
+
+
+# MAIN()
+# Loading image assets
+BG = pygame.image.load("assets/bg_lava.png")
+BG = scale_image(BG, 1.5) # resizing shiz to fit
+
+TRACK = pygame.image.load("assets/track_600.png")
+TRACK_OL = pygame.image.load("assets/track_outline_600.png")
+FINISH = pygame.image.load("assets/finishLine.png")
+FINISH = scale_image(FINISH, 0.2)
+
+CAR_R = pygame.image.load("assets/car_red.png")
+CAR_R = scale_image(CAR_R, 0.2)
+
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("F1 Simulator!🔥🔥")
+
+images = [(BG, (0,0)), (TRACK, (0, 0)), (FINISH, (0, 0)),] # Order of loading assets
+
+
+class AbstractCar:  # Abstract parent class of a car
+    def __init__(self, max_vel, rot_vel):
+        self.img = self.IMG # my car
+        self.max_vel = max_vel
+        self.vel = 0 # starting velo
+        self.rot_vel = rot_vel # how quickly you can turn
+        self.angle = -70 # starting angle
+        self.x, self.y = self.StartPos
+
+    def rotate(self, left = False, right = False):
+        if left:
+            self.angle += self.rot_vel
+        elif right:
+            self.angle -= self.rot_vel
+
+    def draw(self, win):
+        rotate_image_ByCenter(win, self.img, (self.x, self.y), self.angle)
+
+
+class HumanPlayer(AbstractCar): # inherits everything from super class.
+    IMG = CAR_R
+    StartPos = (100, 297)
+
+def draw(win, images, humanCar):
+    for img, posi in images:
+        win.blit(img, posi)
+
+    humanCar.draw(win) # Draw the car
+    pygame.display.update()  # Update display after new drawing
+
+
+human1 = HumanPlayer(4,4)
+
+FPS = 60
+clock = pygame.time.Clock() # To sync FPS ## no jittery
+
+run = True
+while run: # handles all events (collisions, user movements, window status, etc)
+    clock.tick(FPS)  # V-Sync
+
+    # WIN.blit(BG, (0, 0)) # drawing image on the display, 0,0 is the position// top left origin
+    draw(WIN, images, human1) # Drawing
+
+
+    for event in pygame.event.get(): # Any event happened will be handelled here
+        if event.type == pygame.QUIT: # X is clicked?
+            run = False
+            break
+
+pygame.quit()
---
+++ pygame_util.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+def scale_image(img, factor):  # resize the image
+    size = round(img.get_width() * factor), round(img.get_height() * factor)
+    return pygame.transform.scale(img, size)
+
+
+def rotate_image_ByCenter(win, img, top_left, angle):
+    rotImg = pygame.transform.rotate(img, angle) # pygame default rotate at topleft ig...
+    newRect = rotImg.get_rect(center=img.get_rect(topleft=top_left).center) # To avoid morphing/destortion after
+    # rotation, translate the new image back to the new center
+    win.blit(rotImg, newRect) # rotated image with new coordinates.
+
